75e8b82b4c7f81072f82599cec1b6ea94755cf71
JqrqmyChqn <chanjeremy2015@gmail.com>
2024-11-25 04:25:34 -0500

    Preliminary implementation of ProfilePage and EditProfilePanel

--- src/main/java/view/ProfilePageUI/ProfilePage.java
+++ src/main/java/view/ProfilePageUI/ProfilePage.java
@@ -1,13 +1,29 @@
 package view.ProfilePageUI;
 
-//import interface_adapter.profile.ProfileViewModel;
+import interface_adapter.profile.ProfileViewModel;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class ProfilePage extends JFrame{
-//    private final ProfileViewModel viewModel;
-//
-//    public ProfilePage(ProfileViewModel viewModel) {
-//        this.viewModel = viewModel;
-//    }
+public class ProfilePage extends JFrame {
+    private final ProfileViewModel viewModel;
+
+    public ProfilePage(ProfileViewModel viewModel) {
+        this.viewModel = viewModel;
+
+        setTitle("Profile Page");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(800, 600);
+        setLayout(new BorderLayout());
+
+        // Top Panel: Edit Profile
+        EditProfilePanel editProfilePanel = new EditProfilePanel(viewModel);
+        add(editProfilePanel, BorderLayout.NORTH);
+
+//        // Center Panel: User Posts
+//        UserPostsPanel userPostsPanel = new UserPostsPanel(viewModel);
+//        add(userPostsPanel, BorderLayout.CENTER);
+
+        setVisible(true);
+    }
 }
