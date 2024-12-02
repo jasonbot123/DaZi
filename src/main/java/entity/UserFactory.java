5e5ca6f0a6f3856b41d14005a390475d9dcc31d1
Taha Qureshi <taha.hashim@gmail.com>
2024-12-01 21:14:08 -0500

    Fixed Javadoc

--- src/main/java/entity/UserFactory.java
+++ src/main/java/entity/UserFactory.java
@@ -5,10 +5,15 @@ package entity;
  */
 public interface UserFactory {
     /**
-     * Creates a new User.
+     * Creates a new User with the specified details.
      * @param name the name of the new user
-     * @param password the password of the new user
-     * @return the new user
+     * @param bio a brief biography or description of the user
+     * @param college the college the user is affiliated with
+     * @param email the email address of the user
+     * @param password the password for the user account
+     * @param program the academic program the user is enrolled in
+     * @param year the year of study the user is in
+     * @return the newly created User instance
      */
     User create(String name, String bio, String college, String email, String password, String program, String year);
 
--- src/main/java/interface_adapter/change_password/ChangePasswordController.java
+++ src/main/java/interface_adapter/change_password/ChangePasswordController.java
@@ -14,9 +14,14 @@ public class ChangePasswordController {
     }
 
     /**
-     * Executes the Change Password Use Case.
-     * @param password the new password
-     * @param username the user whose password to change
+     * Executes the Change Password Use Case with the provided user details.
+     * @param username the username of the user whose password is to be changed
+     * @param bio a brief biography or description of the user
+     * @param college the college the user is affiliated with
+     * @param email the email address of the user
+     * @param password the new password for the user account
+     * @param program the academic program the user is enrolled in
+     * @param year the year of study the user is in
      */
     public void execute(String username, String bio, String college, String email, String password, String program, String year) {
         final ChangePasswordInputData changePasswordInputData = new ChangePasswordInputData(username, bio, college, email, password, program, year);
