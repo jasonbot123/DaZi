0326e33a373fe7f11186f5897978d4fcbffbde4b
Taha Qureshi <taha.hashim@gmail.com>
2024-12-01 21:36:49 -0500

    Implemented error checking for Login and Signup

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -49,83 +49,8 @@ public class Main {
         application.pack();
         application.setVisible(true);
 
-
-        /*
-        String title = "Post";
-        String content = "test ";
-        String sectionName = "GAMING";
-        String username = "12";
-gi
-        postService.addPost(title, content, sectionName, username);
-
-         */
-
     }
 }
 
-    /*
-        postService.getPostByTitle(title);
 
-        String newContent = "updated content of my first post.";
-        boolean updateSuccess = postService.updatePostContent(title, newContent);
-        System.out.println("Content Update Success: " + updateSuccess);
-
-        // test1
-        userService.signUp("Jason", "test@email.com", "pass123");
-
-        // test2
-        String username = "celine";
-        String email = "celine@exampleemail.com";
-        String password = "celine123";
-        String newPassword = "newPassword123";
-
-        // sign up celine
-        boolean signupSuccess = userService.signUp(username, email, password);
-        System.out.println("Signup Success: " + signupSuccess);
-
-        // check if user exists
-        boolean userExists = userDao.userExists(username);
-        System.out.println("User Exists: " + userExists);
-
-        // try to log in with the correct password
-        boolean loginSuccess = userService.login(username, password);
-        System.out.println("Login Success: " + loginSuccess);
-
-        // try to update the user's password
-        boolean updatePasswordSuccess = userService.updateUserPassword(username, newPassword);
-        System.out.println("Password Update Success: " + updatePasswordSuccess);
-
-        // try to log in with the new password
-        boolean loginNewPasswordSuccess = userService.login(username, newPassword);
-        System.out.println("Login with New Password Success: " + loginNewPasswordSuccess);
-
-        // delete the user
-        boolean deleteSuccess = userService.deleteUser(username);
-        System.out.println("User Deletion Success: " + deleteSuccess);
-
-        // verify user no longer exists
-        boolean userExistsAfterDeletion = userDao.userExists(username);
-        System.out.println("User Exists After Deletion: " + userExistsAfterDeletion);
-
-         */
-
-    /*
-    public static void main(String[] args) {
-
-        final AppBuilder appBuilder = new AppBuilder();
-        final JFrame application = appBuilder
-                                            .addLoginView()
-                                            .addSignupView()
-                                            .addLoggedInView()
-                                            .addSignupUseCase()
-                                            .addLoginUseCase()
-                                            .addChangePasswordUseCase()
-                                            .addLogoutUseCase()
-                                            .build();
-
-        application.pack();
-        application.setVisible(true);
-    }
-
-     */
 
