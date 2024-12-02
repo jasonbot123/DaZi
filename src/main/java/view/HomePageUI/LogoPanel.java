6c1873a05b0ef4104cda21a2ab4dc86a4cef7130
Jason Yang <jzc.yang@mail.utoronto.ca>
2024-12-02 05:06:15 -0500

    Updated Logo

--- src/main/java/view/HomePageUI/LogoPanel.java
+++ src/main/java/view/HomePageUI/LogoPanel.java
@@ -6,8 +6,8 @@ import java.awt.*;
 public class LogoPanel extends JPanel {
     public LogoPanel() {
         setLayout(new FlowLayout(FlowLayout.CENTER)); // Center the logo
-        ImageIcon originalLogo = new ImageIcon(getClass().getResource("/images/logo.png")); // TODO: replace with the right logo
-        Image scaledLogo = originalLogo.getImage().getScaledInstance(150, 100, Image.SCALE_SMOOTH); // Adjust dimensions
+        ImageIcon originalLogo = new ImageIcon(getClass().getResource("/images/Dazi.png")); // TODO: replace with the right logo
+        Image scaledLogo = originalLogo.getImage().getScaledInstance(250, 100, Image.SCALE_SMOOTH); // Adjust dimensions
         JLabel logoLabel = new JLabel(new ImageIcon(scaledLogo));
         add(logoLabel);
         setPreferredSize(new Dimension(200, 120));
