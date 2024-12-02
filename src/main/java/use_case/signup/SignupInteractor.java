21d6201b3f78f5effccfd4e84bcb126084067dab
Taha Qureshi <taha.hashim@gmail.com>
2024-12-01 23:35:26 -0500

    Added uoft authentication

--- src/main/java/use_case/signup/SignupInteractor.java
+++ src/main/java/use_case/signup/SignupInteractor.java
@@ -22,26 +22,30 @@ public class SignupInteractor implements SignupInputBoundary {
 
     @Override
     public void execute(SignupInputData signupInputData) {
-        if(!signupInputData.getUsername().isEmpty() && !signupInputData.getPassword().isEmpty()
-                && !signupInputData.getRepeatPassword().isEmpty() && !signupInputData.getEmail().isEmpty()){
-            if (userDataAccessObject.existsByName(signupInputData.getUsername())) {
-                userPresenter.prepareFailView("User already exists.");
-            }
-            else if (!signupInputData.getPassword().equals(signupInputData.getRepeatPassword())) {
-                userPresenter.prepareFailView("Passwords don't match.");
-            }
-            else {
-                final User user = userFactory.create(signupInputData.getUsername(), signupInputData.getBio(),
-                        signupInputData.getCollege(),signupInputData.getEmail(), signupInputData.getPassword(),
-                        signupInputData.getProgram(), signupInputData.getYear());
-                userDataAccessObject.save(user);
+        if (!signupInputData.getUsername().isEmpty() && !signupInputData.getPassword().isEmpty()
+                && !signupInputData.getRepeatPassword().isEmpty() && !signupInputData.getEmail().isEmpty()) {
 
-                final SignupOutputData signupOutputData = new SignupOutputData(user.getName(), false);
-                userPresenter.prepareSuccessView(signupOutputData);
+            if (signupInputData.getEmail().contains("@mail.utoronto.ca")) {
+                if (userDataAccessObject.existsByName(signupInputData.getUsername())) {
+                    userPresenter.prepareFailView("User already exists.");
+                }
+                else if (!signupInputData.getPassword().equals(signupInputData.getRepeatPassword())) {
+                    userPresenter.prepareFailView("Passwords don't match.");
+                }
+                else {
+                    final User user = userFactory.create(signupInputData.getUsername(), signupInputData.getBio(),
+                            signupInputData.getCollege(),signupInputData.getEmail(), signupInputData.getPassword(),
+                            signupInputData.getProgram(), signupInputData.getYear());
+                    userDataAccessObject.save(user);
 
-            }
-        }else{
-                userPresenter.prepareFailView("Please fill out all fields.");
+                    final SignupOutputData signupOutputData = new SignupOutputData(user.getName(), false);
+                    userPresenter.prepareSuccessView(signupOutputData);
+
+                }
+            }else{
+                userPresenter.prepareFailView("Must have a University of Toronto email.");
+            } }else{
+            userPresenter.prepareFailView("Please fill out all fields.");
         }
 
     }
